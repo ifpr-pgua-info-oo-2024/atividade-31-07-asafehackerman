529272ee3804f97234fa1afe87842c41babccf24
Asafe <133934794+asafehackerman@users.noreply.github.com>
2024-07-31 12:27:48 -0300

    Update Academia.java

--- Academia.java
+++ Academia.java
@@ -9,28 +9,28 @@ public class Academia {
         this.telefone = telefone;
     }
 
-        public String getNome() {
-            return this.nome;
-        }
+    public String getNome() {
+        return this.nome;
+    }
 
-        public String getEndereco() {
-            return this.endereco;
-        }
+    public String getEndereco() {
+        return this.endereco;
+    }
 
-        public String getTelefone() {
-            return this.telefone;
-        }
+    public String getTelefone() {
+        return this.telefone;
+    }
 
-            public void setNome(String nome) {
-                this.nome = nome;
-            }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-            public void setEndereco(String endereco) {
-                this.endereco = endereco;
-            }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 
-            public void setTelefone(String telefone) {
-                this.telefone = telefone;
-            }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 
 }
